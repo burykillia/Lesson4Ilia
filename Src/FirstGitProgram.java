8ba7bb3dd720b8518461aeb96e248b2f162c4db0
buryk <buryk.illya@gmail.com>
2023-06-02 20:40:06 +0300

    first commit

--- Src/FirstGitProgram.java
+++ Src/FirstGitProgram.java
@@ -2,6 +2,6 @@ public class FirstGitProgram {
 
     public static  void main(String[] args){
 
-        System.out.println("Hello Git");
+        System.out.println("Hello Git I am Ilia future program god");
     }
 }
