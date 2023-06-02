c66621e929acf4c960cadecaa7cb28c82af18dad
buryk <buryk.illya@gmail.com>
2023-06-02 20:48:22 +0300

    Third comit

--- Src/FirstGitProgram.java
+++ Src/FirstGitProgram.java
@@ -3,5 +3,7 @@ public class FirstGitProgram {
     public static  void main(String[] args){
 
         System.out.println("Hello Git I am Ilia ");
+        int a = 89898;
+        System.out.println(a);
     }
 }
